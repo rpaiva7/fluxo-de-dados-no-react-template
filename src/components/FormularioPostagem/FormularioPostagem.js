0e2fc370cb71bbe7d19fa256b62fb5bd16a3d02f
Renan Paiva <rpaivadev@gmail.com>
2023-06-11 22:43:59 -0300

    react-fluxo-de-dados

--- src/components/FormularioPostagem/FormularioPostagem.js
+++ src/components/FormularioPostagem/FormularioPostagem.js
@@ -1,6 +1,21 @@
 import { FormContainer, Form, Input, StyledLabel } from "./FormularioPostagem.styled";
+import { useState } from "react";
 
-export const FormularioCadastro = () => {
+export const FormularioCadastro = (props) => {
+
+
+  const trocarImagem = (event) => {
+    props.setImagem(event.target.value)/*  Posso usar apenas a letra e no lugar da palavra event do set */
+  }
+
+  const trocarDescricao = (event) => {
+    props.setDescricao(event.target.value)
+  }
+
+  const trocarTitulo = (event) => {
+    props.setTitulo(event.target.value)
+  }
+
   return (
     <FormContainer>
       <h1>Insira sua postagem abaixo: </h1>
@@ -8,17 +23,24 @@ export const FormularioCadastro = () => {
       <Form>
         <StyledLabel htmlFor="titulo">
           Titulo:
-          <Input id="titulo" />
+          <Input id="titulo" 
+          value={props.titulo}
+          onChange={trocarTitulo} />
         </StyledLabel>
 
         <StyledLabel htmlFor="foto">
           Imagem:
-          <Input id="foto" />
+          <Input id="foto"
+            value={props.imagem}
+            onChange={trocarImagem}
+          />
         </StyledLabel>
 
         <StyledLabel htmlFor="descricao">
           Descrição:
-          <Input id="descricao" />
+          <Input id="descricao"
+            value={props.descricao}
+            onChange={trocarDescricao} />
         </StyledLabel>
       </Form>
     </FormContainer>
--- src/components/TelaDaPostagem/TelaDaPostagem.js
+++ src/components/TelaDaPostagem/TelaDaPostagem.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { ContainerPostagem, Title, Image, Description } from "./TelaDaPostagem.styled";
 
-const TelaDaPostagem = () => {
+const TelaDaPostagem = (props) => {
+
+  /* Essa é uma forma de chamar o ternário no JavaScript. Cada <ContainerPostagem> é uma condição do ternário que criei abaixo (props.imagem.length > 0 && props.descricao.length > 0) ? */
+
   return (
-    <ContainerPostagem>
-      <Title>Titulo</Title>
-      <Image src={"https://picsum.photos/536/354"} />
-      <Description>Descrição</Description>
-    </ContainerPostagem>
+    (props.imagem.length > 0 && props.descricao.length > 0) ?
+      <ContainerPostagem>
+        <Title>{props.titulo}</Title>
+        <Image src={props.imagem} alt='drone view' />
+        <Description>{props.descricao}</Description>
+      </ContainerPostagem> :
+
+      <ContainerPostagem>
+        <Image src={"https://picsum.photos/536/354"} alt='drone view' />
+        <Description>{'Lorem Ipsumr'}</Description>
+      </ContainerPostagem>
   );
 };
 
 export default TelaDaPostagem;
+
+
